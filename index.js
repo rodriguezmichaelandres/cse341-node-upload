489f4c20658c79ddbf7a1222e03ee991d102e024
rodriguezmichaelandres <andresrodrock@gmail.com>
2021-10-13 15:40:14 -0500

    con index 4.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 const errorController = require('./controllers/error');
@@ -14,9 +14,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const mainRoutes = require('./routes/mainRoute');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -30,12 +32,15 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use('/prove02', mainRoutes)
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 mongoose
   .connect('mongodb+srv://andresrodrock:200694@testcluster.h472u.mongodb.net/shop?retryWrites=true&w=majority')
